3847e4082f841c8c4315777df880ac0ad460277a
Lucas <lucasporteret@gmail.com>
2024-07-24 18:41:57 +0200

    8

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: '',
-    clientSecret: '',
+    clientId: '2464c3b3b9c9452b9435682691b58831',
+    clientSecret: 'e5160698201c4a07a83cf05c77ce15aa',
     redirectUri: 'https://lucky.freeboxos.fr/callback'
 });
 
@@ -81,7 +81,7 @@ app.get('/last-winner', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 app.post('/set-players', (req, res) => {
