3dc07b50ca17b926283af4f05f99d32d0d41002c
Lucas Porteret <115562805+Lucky7495@users.noreply.github.com>
2025-01-08 09:52:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -303,5 +303,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
